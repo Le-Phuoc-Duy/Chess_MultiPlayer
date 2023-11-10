0f984d3ae3373927b43aef1acf546d12c7c95d95
phuocduy <phuocduylpd108@gmail.com>
2023-11-10 15:47:00 +0700

    feat: play with friend, client

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -20,7 +20,7 @@ window.onload = function(){
     console.log("tao game thanh cong")
     init(game.board)
 }
-const socket = new SockJS('http://localhost:8888/ws');  
+const socket = new SockJS('http://localhost:8888/ws');   
 const stompClient = new Client({
     webSocketFactory: () => socket,
     // connectHeaders: {
@@ -173,29 +173,29 @@ document.getElementById("registerButton")?.addEventListener("click",async () => 
             '<div class="container">' +
             '  <div class="row">' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img1" name="image" value="img1" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img1" name="image" value="img1" style="display: none;">' +
             '      <label class="ava" for="img1"><img src="./assets/ava01.png" style="width: 90%;"></label>' +
             '    </div>' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img2" name="image" value="img2" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img2" name="image" value="img2" style="display: none;">' +
             '      <label class="ava" for="img2"><img src="./assets/ava02.png" style="width: 90%;"></label>' +
             '    </div>' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img3" name="image" value="img3" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img3" name="image" value="img3" style="display: none;">' +
             '      <label class="ava" for="img3"><img src="./assets/ava03.png" style="width: 90%;"></label>' +
             '    </div>' +
             '  </div>' +
             '  <div class="row">' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img4" name="image" value="img4" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img4" name="image" value="img4" style="display: none;">' +
             '      <label class="ava" for="img4"><img src="./assets/ava04.png" style="width: 90%;"></label>' +
             '    </div>' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img5" name="image" value="img5" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img5" name="image" value="img5" style="display: none;">' +
             '      <label class="ava" for="img5"><img src="./assets/ava05.png" style="width: 90%;"></label>' +
             '    </div>' +
             '    <div class="col-md-4">' +
-            '      <input type="radio" id="img6" name="image" value="img6" style="display: none;">' +
+            '      <input class="avaInput" type="radio" id="img6" name="image" value="img6" style="display: none;">' +
             '      <label class="ava" for="img6"><img src="./assets/ava06.png" style="width: 90%;"></label>' +
             '    </div>' +
             '  </div>' +
@@ -223,4 +223,43 @@ document.getElementById("registerButton")?.addEventListener("click",async () => 
     if (formValues) {
         console.log("username: " + formValues[0] + ", password: " + formValues[1] + ", avatar: " + formValues[2])          // Swal.fire(`Tên người dùng: ${formValues[0]}<br>Mật khẩu: ${formValues[1]}`)
     }
-})
+}) 
+document.getElementById("playWithFriend")?.addEventListener("click",async () => {
+    const inputOptions = new Promise((resolve) => {
+        resolve({
+            "joinRoom": "Phòng có sẵn",
+            "createRoom": "Tạo phòng"
+        }); 
+      });
+    const { value: option } = await Swal.fire({
+        title: "Chơi với bạn",
+        input: "radio",
+        inputOptions,
+        inputValidator: (value) => {
+          if (!value) {
+            return "Nhập lựa chọn!";
+        }
+        }
+      });
+      if (option) {
+        if(option === "joinRoom"){
+            const { value: idRoom } = await Swal.fire({
+                input: "text",
+                title: "Mã phòng",
+                inputPlaceholder: "Nhập mã phòng",
+                inputValidator: (value) => {
+                    if (!value) {
+                      return "Nhập mã phòng!";
+                }
+            }
+            });
+            if (idRoom) {
+                Swal.fire(`Entered URL: ${idRoom}`);
+            } 
+        }
+        if(option === "createRoom"){
+            Swal.fire({ html: `You selected: option === "createRoom"` });
+
+        }
+      }
+}) 
